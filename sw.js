afd9fad413f08643153dea0ca62c5ccbe9d1c165
markosmmp <markosmpbr@gmail.com>
2026-09-16 22:25:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='psych-admin-v1.3';
+const CACHE='psych-admin-v1.4';
 const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./js/api.js','./js/data.js','./js/render.js','./js/utils.js','./manifest.json','./assets/icons/favicon.svg','./assets/icons/app-icon.svg','./assets/images/brand-card.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
